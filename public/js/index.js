dd66c1d59e317f040d72fb538071ef2482c63f57
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2022-07-05 18:59:45 -0500

    Layout de página principal para móvil

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,9 @@
 const API_URL="https://api.themoviedb.org/3/movie/76341?api_key="
 console.log(`${API_URL}${API_KEY}`);
+
+const button = document.querySelector('#show-menu');
+button.onclick = () => {
+    let menu = document.querySelector('#menu');
+    menu.classList.toggle('hide');
+    menu.classList.toggle('show');
+}
